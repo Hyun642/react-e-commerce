e6947d5ab41c736eb5b4ecd1ac3bb8cae05bb5ba
Hyun <jjjtoy@naver.com>
2025-09-08 17:55:56 +0900

    feat: signup api

---
+++ src/api/auth/signup.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const signup = async (name, email, password, phoneNumber) => {
+     try {
+          const res = await axios.post(`${process.env.REACT_APP_SERVER_URL}/user/signup`, {
+               name,
+               email,
+               password,
+               phoneNumber,
+          });
+          return res;
+     } catch (error) {
+          console.error("signup: ", error.response.data);
+          return error.response;
+     }
+};
